f3127e32c6c835a38ffd623afae1470bbba1532e
vkado <wee_weerapat@hotmail.com>
2014-07-28 20:57:15 +0700

    update support new feature

--- app.js
+++ app.js
@@ -34,6 +34,10 @@ window.onload = function () {
         console.log("pb.point");
         console.log(result);
     });
+    pb.pointHistory('1', 'exp', 0, 10, function (result) {
+        console.log("pb.pointHistory");
+        console.log(result);
+    });
     pb.actionLastPerformed('1', function (result) {
         console.log("pb.actionLastPerformed");
         console.log(result);
@@ -54,6 +58,30 @@ window.onload = function () {
         console.log("pb.rank");
         console.log(result);
     });
+    pb.ranks(10, function (result) {
+        console.log("pb.ranks");
+        console.log(result);
+    });
+    pb.level(10, function (result) {
+        console.log("pb.level");
+        console.log(result);
+    });
+    pb.levels(function (result) {
+        console.log("pb.levels");
+        console.log(result);
+    });
+    pb.goodsOwned(1, function (result) {
+        console.log("pb.goodsOwned");
+        console.log(result);
+    });
+    pb.questOfPlayer(1, "53903b37988040851b8b457a", function (result) {
+        console.log("pb.questOfPlayer");
+        console.log(result);
+    });
+    pb.questListOfPlayer(1, function (result) {
+        console.log("pb.questListOfPlayer");
+        console.log(result);
+    });
     pb.badges(function (result) {
         console.log("pb.badges");
         console.log(result);
@@ -62,6 +90,43 @@ window.onload = function () {
         console.log("pb.badge");
         console.log(result);
     });
+    pb.goodsList(function (result) {
+        console.log("pb.goodsList");
+        console.log(result);
+    });
+    pb.goods("535f1f7b988040832a8b4589", function (result) {
+        console.log("pb.goods");
+        console.log(result);
+    });
+    pb.quests(function (result) {
+        console.log("pb.quests");
+        console.log(result);
+    });
+    pb.quest("53903b37988040851b8b457a", function (result) {
+        console.log("pb.quest");
+        console.log(result);
+    });
+    pb.mission("53903b37988040851b8b457a", "53903b37988040851b8b4576", function (result) {
+        console.log("pb.mission");
+        console.log(result);
+    });
+    pb.questsAvailable(1, function (result) {
+        console.log("pb.questsAvailable");
+        console.log(result);
+    });
+    pb.questAvailable("53903b37988040851b8b457a", 1, function (result) {
+        console.log("pb.questAvailable");
+        console.log(result);
+    });
+    pb.recentPoint(0, 10, function (result) {
+        console.log("pb.recentPoint");
+        console.log(result);
+    });
+    pb.recentPointByName("exp", 0, 10, function (result) {
+        console.log("pb.recentPointByName");
+        console.log(result);
+    });
+
     pb.actionConfig(function (result) {
         console.log("pb.actionConfig");
         console.log(result);
